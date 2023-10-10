1368beb4319622aa06f151594c417376dc78fb47
Yehor Akunishnikov <yehorakunishnikov@gmail.com>
2023-10-10 06:22:10 +0300

    Refactor with class

--- script.js
+++ script.js
@@ -1,75 +1,210 @@
 document.addEventListener('DOMContentLoaded', () => {
-const tagsInput = document.querySelector('#container');
-const tagsList = tagsInput.querySelector('.tags');
-const inputContainer = document.querySelector('.input-container');
-const mergeButton = document.querySelector('#merge-button');
-const applyButton = document.querySelector('#apply-merge');
-const cancelButton = document.querySelector('#cancel-merge');
-const resultBtn = document.querySelector('#result');
-const inputField = document.querySelector('.input-field');
-const resultText = document.querySelector('#resultText');
-
-let tags = [];
-let paringBuffer = [];
-let mergeMode = false;
-
-resultBtn.addEventListener('click', () => {
-  console.log(
-    tags.map((tagText) => {
-      const tagParts = tagText.split('/');
-
-      if (tagParts.length > 1) {
-        return { text: tagText, type: 'paring' };
-      } else {
-        return { text: tagText, type: 'character' };
-      }
-    })
-  );
-
-  resultText.innerText = tags.join('; ');
-});
-
-const createTag = (text, isParing = false) => {
-  const tag = document.createElement('li');
-  const closeBtn = document.createElement('span');
-  const content = document.createElement('span');
-
-  tag.classList.add('tag');
-
-  if (isParing) {
-    tag.classList.add('paring');
+class ParingsInput {
+  constructor() {
+    this.initState();
+    this.setDomNodes();
+    this.setListeners();
   }
 
-  content.classList.add('tag-content');
-  content.append(text);
+  initState() {
+    this.tags = [];
+    this.paringBuffer = [];
+    this.isMergeMode = false;
+  }
 
-  closeBtn.classList.add('remove-tag');
-  closeBtn.innerText = '✖';
+  setDomNodes() {
+    this.tagsInput = document.querySelector('#container');
+    this.tagsListElement = this.tagsInput.querySelector('.tags');
+    this.inputContainer = document.querySelector('.input-container');
+    this.mergeButton = document.querySelector('#merge-button');
+    this.applyButton = document.querySelector('#apply-merge');
+    this.cancelButton = document.querySelector('#cancel-merge');
+    this.resultBtn = document.querySelector('#result');
+    this.inputField = document.querySelector('.input-field');
+    this.resultText = document.querySelector('#resultText');
+    this.mergeIcon = document.querySelector('.merge-icon');
+  }
 
-  tag.append(closeBtn);
-  tag.append(content);
-
-  return tag;
-};
-
-mergeButton.addEventListener('click', (e) => {
-  if (tags.length) {
-    e.target.classList.add('hidden');
-    mergeMode = true;
-    applyButton.classList.remove('hidden');
-    cancelButton.classList.remove('hidden');
-    inputField.setAttribute('disabled', true);
-    tagsList.setAttribute(
-      'title',
-      'Обери мінімум два персонажі, щоб створити пейринг'
+  setListeners() {
+    this.resultBtn.addEventListener('click', (e) => this.onResultClick(e));
+    this.mergeButton.addEventListener('click', (e) => this.onMergeClick(e));
+    this.applyButton.addEventListener('click', (e) => this.onApplyClick(e));
+    this.inputField.addEventListener('keyup', (e) => this.onKeyUp(e));
+    this.tagsListElement.addEventListener('click', (e) =>
+      this.onTagsListClick(e)
     );
+    this.cancelButton.addEventListener('click', () => {
+      this.onCancelClick();
+    });
   }
-});
 
-applyButton.addEventListener('click', (e) => {
-  if (paringBuffer.length) {
-    const tagsToMerge = tagsList.querySelectorAll('.to-merge');
-    const paringText = paringBuffer
+  onKeyUp(e) {
+    const keyCode = e.keyCode;
+    const value = this.inputField.value.replace(
+      /[^a-zA-Z0-9А-ЩЬЮЯҐЄІЇа-щьюяґєії'` ]/g,
+      ''
+    );
+    const isDuplicate = this.tags.some((tagText) => tagText === value);
+
+    if (keyCode === 13 && value && !isDuplicate) {
+      this.tags.push(value);
+      this.tagsListElement.insertBefore(
+        this.createTag(value),
+        this.inputContainer
+      );
+      this.inputField.value = null;
+
+      const charactersCount = this.tags.filter(
+        (tagText) => !tagText.includes('/')
+      ).length;
+
+      if (charactersCount > 1) {
+        this.mergeButton.classList.remove('hidden');
+      }
+    }
+  }
+
+  onTagClick(tag) {
+    const isParing = tag.classList.contains('paring');
+
+    if (this.isMergeMode && !isParing) {
+      const isMarkedToMerge = tag.classList.contains('to-merge');
+      const text = tag.querySelector('.tag-content').innerText;
+
+      if (isMarkedToMerge) {
+        tag.classList.remove('to-merge');
+        this.paringBuffer = this.paringBuffer.filter(
+          (paringItem) => paringItem !== text
+        );
+      } else {
+        tag.classList.add('to-merge');
+        this.paringBuffer.push(text);
+      }
+
+      if (
+        this.paringBuffer.length > 1 &&
+        this.tags.some(
+          (paring) => paring === this.createParingText(this.paringBuffer)
+        )
+      ) {
+        this.inputField.setAttribute('placeholder', `Такий пейринг вже додано`);
+      } else {
+        this.inputField.setAttribute(
+          'placeholder',
+          `Персонажів у пейрингу: ${this.paringBuffer.length}`
+        );
+      }
+    }
+  }
+
+  onRemoveClick(tag) {
+    const text = tag.querySelector('.tag-content').innerText;
+
+    this.tags = this.tags.filter((tagText) => tagText !== text);
+    tag.remove();
+
+    const charactersCount = this.tags.filter(
+      (tagText) => !tagText.includes('/')
+    ).length;
+
+    if (charactersCount <= 1) {
+      this.mergeButton.classList.add('hidden');
+    }
+  }
+
+  onTagsListClick(e) {
+    const elementType = this.identifyElementOnBubbling(e.target);
+
+    const tag = this.getTag(e.target, elementType);
+
+    if (tag) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    switch (elementType) {
+      case 'tag':
+      case 'tagContent': {
+        this.onTagClick(tag, this.isMergeMode, this.paringBuffer);
+        break;
+      }
+      case 'removeButton': {
+        this.onRemoveClick(tag);
+        break;
+      }
+    }
+  }
+
+  onApplyClick() {
+    if (this.paringBuffer.length) {
+      const paringText = this.createParingText(this.paringBuffer);
+      const isDuplicate = this.tags.some((tagText) => tagText === paringText);
+
+      if (!isDuplicate) {
+        this.tagsListElement.prepend(this.createTag(paringText, true));
+        this.tags = [paringText, ...this.tags];
+
+        this.disableMergeMode();
+      }
+    }
+  }
+
+  onCancelClick() {
+    this.disableMergeMode();
+  }
+
+  onMergeClick() {
+    if (this.tags.length > 1) {
+      this.enableMergeMode();
+    }
+  }
+
+  onResultClick() {
+    console.log(
+      this.tags.map((tagText) => {
+        const tagParts = tagText.split('/');
+
+        if (tagParts.length > 1) {
+          return { text: tagText, type: 'paring' };
+        } else {
+          return { text: tagText, type: 'character' };
+        }
+      })
+    );
+
+    this.resultText.innerText = this.tags.join('; ');
+  }
+
+  getTag(element, elementType) {
+    switch (elementType) {
+      case 'tag':
+        return element;
+      case 'tagContent':
+      case 'removeButton':
+        return element.parentNode;
+      default:
+        return null;
+    }
+  }
+
+  identifyElementOnBubbling(element) {
+    if (element.classList.contains('tag')) {
+      return 'tag';
+    }
+
+    if (element.classList.contains('tag-content')) {
+      return 'tagContent';
+    }
+
+    if (element.classList.contains('remove-tag')) {
+      return 'removeButton';
+    }
+
+    return null;
+  }
+
+  createParingText(parings) {
+    return parings
       .sort((prev, next) => {
         if (prev.toLowerCase() < next.toLowerCase()) {
           return -1;
@@ -80,104 +215,65 @@ applyButton.addEventListener('click', (e) => {
         return 0;
       })
       .join('/');
-
-    if (!tags.some((tagText) => tagText === paringText)) {
-      tagsList.prepend(createTag(paringText, true));
-      tags = [paringText, ...tags];
-
-      tagsToMerge.forEach((tag) => {
-        tag.classList.remove('to-merge');
-      });
-
-      applyButton.classList.add('hidden');
-      cancelButton.classList.add('hidden');
-      mergeButton.classList.remove('hidden');
-      inputField.removeAttribute('disabled');
-      tagsList.setAttribute('title', '');
-      mergeMode = false;
-      paringBuffer = [];
-    }
-  }
-});
-
-cancelButton.addEventListener('click', () => {
-  tagsList.querySelectorAll('.tag').forEach((tag) => {
-    tag.classList.remove('to-merge');
-  });
-
-  paringBuffer = [];
-  applyButton.classList.add('hidden');
-  cancelButton.classList.add('hidden');
-  mergeButton.classList.remove('hidden');
-  inputField.removeAttribute('disabled');
-  tagsList.setAttribute('title', '');
-  mergeMode = false;
-});
-
-tagsInput.addEventListener('click', (e) => {
-  const element = e.target;
-
-  if (
-    element.classList.contains('tag') ||
-    element.classList.contains('tag-content') ||
-    element.classList.contains('remove-tag')
-  ) {
-    e.preventDefault();
-    e.stopPropagation();
   }
 
-  if (mergeMode) {
-    if (element.classList.contains('tag')) {
-      if (!element.classList.contains('paring')) {
-        element.classList.toggle('to-merge');
-      }
-    }
+  enableMergeMode() {
+    this.tagsListElement.querySelectorAll('.tag').forEach((tag) => {
+      tag.setAttribute('title', 'додати/прибрати з пейрингу');
+      tag.querySelector('.remove-tag').classList.add('hidden');
+    });
 
-    if (element.classList.contains('tag-content')) {
-      if (!element.parentNode.classList.contains('paring')) {
-        const isMarkedToMerge =
-          element.parentNode.classList.contains('to-merge');
-        const text = element.parentNode.querySelector('.tag-content').innerText;
-
-        if (isMarkedToMerge) {
-          element.parentNode.classList.remove('to-merge');
-          paringBuffer = paringBuffer.filter(
-            (paringItem) => paringItem !== text
-          );
-        } else {
-          element.parentNode.classList.add('to-merge');
-          paringBuffer.push(text);
-        }
-      }
-    }
+    this.tagsListElement.classList.add('merge-mode');
+    this.mergeButton.classList.add('hidden');
+    this.applyButton.classList.remove('hidden');
+    this.cancelButton.classList.remove('hidden');
+    this.mergeIcon.classList.remove('hidden');
+    this.inputField.setAttribute('placeholder', 'Персонажів у пейрингу: 0');
+    this.inputField.setAttribute('disabled', true);
+    this.isMergeMode = true;
   }
 
-  if (element.classList.contains('remove-tag')) {
-    if (!mergeMode) {
-      const text = element.parentNode.querySelector('.tag-content').innerText;
+  disableMergeMode() {
+    this.tagsListElement.querySelectorAll('.tag').forEach((tag) => {
+      tag.classList.remove('to-merge');
+      tag.querySelector('.remove-tag').classList.remove('hidden');
+      tag.removeAttribute('title');
+    });
 
-      tags = tags.filter((tagText) => tagText !== text);
-
-      element.parentNode.remove();
-    }
+    this.tagsListElement.classList.remove('merge-mode');
+    this.applyButton.classList.add('hidden');
+    this.cancelButton.classList.add('hidden');
+    this.mergeButton.classList.remove('hidden');
+    this.mergeIcon.classList.add('hidden');
+    this.inputField.setAttribute('placeholder', 'Додати персонажа');
+    this.inputField.removeAttribute('disabled');
+    this.isMergeMode = false;
+    this.paringBuffer = [];
   }
-});
 
-tagsInput.addEventListener('keyup', (e) => {
-  const element = e.target;
+  createTag(text, isParing = false) {
+    const tag = document.createElement('li');
+    const closeBtn = document.createElement('button');
+    const content = document.createElement('span');
 
-  if (element.classList.contains('input-field')) {
-    if (e.keyCode == 13) {
-      const value = e.target.value;
+    tag.classList.add('tag');
 
-      if (value) {
-        tags.push(value);
-
-        tagsList.insertBefore(createTag(value), inputContainer);
-
-        element.value = null;
-      }
+    if (isParing) {
+      tag.classList.add('paring');
     }
+
+    content.classList.add('tag-content');
+    content.append(text);
+
+    closeBtn.classList.add('remove-tag');
+    closeBtn.innerText = '✖';
+
+    tag.append(closeBtn);
+    tag.append(content);
+
+    return tag;
   }
-});
+}
+
+new ParingsInput();
 });
